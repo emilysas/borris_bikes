f28dced04e7a351d8b2a2409ca2ea8afacfe7088
Danielle Demkiw <ddemkiw@gmail.com>
2014-12-09 13:08:42 +0000

    Passed .break! test

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,6 +1,16 @@
+
+
+require 'bike'
+
 describe Bike do
   it "should not be broken" do
     bike = Bike.new
-    expect bike.not_to_be_broken
+    expect(bike).not_to be_broken
+  end
+  it "can be broken" do
+  	bike = Bike.new
+  	bike.break!
+  	expect(bike).to be_broken
   end
 end
+
