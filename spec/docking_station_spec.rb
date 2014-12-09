5417e28348023e6204fabc521618997c71b52823
Emily Sas <emily.bronwen.sas@gmail.com>
2014-12-09 14:22:06 +0000

    testing for full station

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -9,10 +9,18 @@ describe DockingStation do
     station.dock(bike)
     expect(station.bike_count).to eq(1)
   end
+
 	it "should release a bike" do 
 	 station.dock(bike)
 	 expect(station.bike_count).to eq(1)
 	 station.release(bike)
 	 expect(station.bike_count).to eq(0)
   end 
+
+  it "should know when it's full" do
+    expect(station).not_to be_full
+    25.times {station.dock(Bike.new)}
+    expect(station).to be_full
+  end
+
 end
