b0cc8ee7eb2077183940695dc01dba742bdc5fe3
Danielle Demkiw <ddemkiw@gmail.com>
2014-12-09 14:12:27 +0000

    Test to release bikes from dock

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,13 +1,18 @@
-
-describe Station do
+require 'docking_station'
+describe DockingStation do
  
- let (:station) {Station.new}
- let (:bike) {Bike.new}
+ let(:station) {DockingStation.new}
+ let(:bike) {Bike.new}
   
-  it "should accept bikes"
+  it "should accept bikes" do
     expect(station.bike_count).to eq(0)
     station.dock(bike)
     expect(station.bike_count).to eq(1)
   end
-	
+	it "should release a bike" do 
+	 station.dock(bike)
+	 expect(station.bike_count).to eq(1)
+	 station.release(bike)
+	 expect(station.bike_count).to eq(0)
+  end 
 end
