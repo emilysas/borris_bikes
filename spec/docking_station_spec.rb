36190deaf0ccd33e9c3b49bb53c8a614acf7c4f5
Emily Sas <emily.bronwen.sas@gmail.com>
2014-12-10 18:54:38 +0000

    adding doubling to docking_station_spec.rb

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,8 @@
 require 'docking_station'
 describe DockingStation do
  
- let(:station) {DockingStation.new(:capacity =>25)}
- let(:bike) {Bike.new}
+ let(:station) {double :station, capacity: 25}
+ let(:bike) {double :bike}
   
   it "should allow setting default capacity on initializing" do
     expect(station.capacity).to eq(25)
