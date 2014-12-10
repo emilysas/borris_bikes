41d3b21b77c52dc321f872aeff5effa06808a742
Emily Sas <emily.bronwen.sas@gmail.com>
2014-12-10 18:44:46 +0000

    passed test for van picking up broken bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -5,9 +5,15 @@ class DockingStation
 
 include BikeContainer 
 
+@@stations = []
 
  def initialize(options = {})
     self.capacity = options.fetch(:capacity, capacity)
+	@@stations << self  
+  end
+
+  def self.stations
+  	@@stations
   end
 end
 
--- lib/van.rb
+++ lib/van.rb
@@ -3,10 +3,15 @@ require_relative 'bike_container'
 require_relative 'docking_station'
 
 class Van
-include BikeContainer
+  include BikeContainer
+  include ObjectSpace
 
   def initialize(options = {})
   	self.capacity=(options.fetch(:capacity, capacity))
+  end
+
+  def station_pickup(bike)
+  	self.dock(bike) if bike.broken?
   end
 
 
