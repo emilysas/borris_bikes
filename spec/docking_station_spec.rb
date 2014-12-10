be3037e4ac33658a1acccf96e876af1e8bcedd51
Emily Sas <emily.bronwen.sas@gmail.com>
2014-12-10 16:41:35 +0000

    adding refactored docking_station_spec

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,36 +4,8 @@ describe DockingStation do
  let(:station) {DockingStation.new(:capacity =>25)}
  let(:bike) {Bike.new}
   
-  it "should accept bikes" do
-    expect(station.bike_count).to eq(0)
-    station.dock(bike)
-    expect(station.bike_count).to eq(1)
-  end
-
-	it "should release a bike" do 
-	 station.dock(bike)
-	 expect(station.bike_count).to eq(1)
-	 station.release(bike)
-	 expect(station.bike_count).to eq(0)
-  end 
-
-  it "should know when it's full" do
-    expect(station).not_to be_full
-    25.times {station.dock(Bike.new)}
-    expect(station).to be_full
-  end
-
-  it "should not accept a bike if it's full" do
-    25.times { station.dock(Bike.new) }
-    expect(lambda { station.dock(bike) }).to raise_error(RuntimeError, 'Station is full')
-  end
-
-  it "should provide a list of available bikes" do
-    working_bike, broken_bike = Bike.new, Bike.new
-    broken_bike.break!
-    station.dock(working_bike)
-    station.dock(broken_bike)
-    expect(station.available_bikes).to eq([working_bike])
+  it "should allow setting default capacity on initializing" do
+    expect(station.capacity).to eq(25)
   end
 
 end
